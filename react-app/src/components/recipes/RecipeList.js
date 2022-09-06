ea3afa77b14ea5fd51d0156390b2674346a50519
allan <ayang740@gmail.com>
2022-09-05 22:15:06 -0400

    recipe list css

--- react-app/src/components/recipes/RecipeList.js
+++ react-app/src/components/recipes/RecipeList.js
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux"
 import RecipeCard from "./RecipeCard"
+import './recipes.css'
 
 export default function RecipeList() {
     const allRecipes = useSelector(state => Object.values(state.recipes.normalizedRecipes).reverse())
     return (
-        <div>
+        <div className="recipe-list-container">
             {allRecipes && allRecipes.map(recipe => (
                 <RecipeCard key={recipe.id} recipe={recipe} />
             ))}
