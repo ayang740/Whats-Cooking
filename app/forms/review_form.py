cb07160ce9736a18ca429e1088f62ab1e82366d4
allan <ayang740@gmail.com>
2022-09-07 00:09:32 -0400

    reviews read, add, delete

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -5,7 +5,7 @@ from app.models import Review
 
 def review_length(form, field):
     review = field.data
-    if len(review) < 10 or len(name) > 1000:
+    if len(review) < 10 or len(review) > 1000:
         raise ValidationError('Review must be between 10 and 1000 characters')
 def rating_limit(form, field):
     rating = field.data
