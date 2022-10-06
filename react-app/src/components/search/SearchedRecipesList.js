e54646d1308d37bf64ab0db4b4a93c3182b94a16
allan <ayang740@gmail.com>
2022-10-06 16:17:25 -0400

    additional search location

--- react-app/src/components/search/SearchedRecipesList.js
+++ react-app/src/components/search/SearchedRecipesList.js
@@ -1,19 +1,24 @@
 import { useEffect } from "react"
 import { useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
+import { useHistory, useParams } from "react-router-dom"
 import RecipeCard from "../recipes/RecipeCard"
+import Search from "./Search"
 
 export default function SearchedRecipesList() {
     const { search } = useParams()
+    const history = useHistory()
     const allRecipes = useSelector(state => Object.values(state.recipes.normalizedRecipes).reverse())
-    console.log(allRecipes)
     const searchedRecipes = allRecipes.filter(recipe => recipe.name.toLowerCase().includes(search))
-    console.log(searchedRecipes)
+
     useEffect(() => {
         window.scrollTo(0,0)
     },[])
+
     return(
         <div className="recipe-list-wrapper">
+            <div className="search-container">
+                <Search />
+            </div>
             <div className="recipe-list-container">
                 {searchedRecipes && searchedRecipes.map(recipe => (
                     <RecipeCard key={recipe.id} recipe={recipe} />
